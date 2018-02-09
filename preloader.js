b334c3739576c564dec1ed77aac1ebe3626d9eba
Mihail <serov.mikhail.job@gmail.com>
2018-02-09 23:39:16 +0300

    : (

--- preloader.js
+++ preloader.js
@@ -1,12 +1,14 @@
 var hellopreloader = document.getElementsByClassName("loader-container")[0];
+var preloader = document.getElementsByClassName("loader")[0];
+
 function fadeOutnojquery(el) {
     el.style.opacity = 1;
     var interhellopreloader = setInterval(
         function () {
-            el.style.opacity = el.style.opacity - 0.025;
-            if (el.style.opacity <= 0.025) {
+            el.style.opacity = el.style.opacity - 0.05;
+            if (el.style.opacity <= 0.05) {
                 clearInterval(interhellopreloader);
-                hellopreloader.style.display = "none";
+                preloader.style.display = "none";
             }
         }, 16);
 }
