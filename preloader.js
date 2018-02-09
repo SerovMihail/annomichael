41b8bfbfb4dfad1c4f0f72cab70cc75277792c3a
Mihail <serov.mikhail.job@gmail.com>
2018-02-09 23:36:21 +0300

    add new preloader for mobile and desctop version. fix calendar style

--- preloader.js
+++ preloader.js
@@ -1,17 +1,19 @@
-var hellopreloader = document.getElementById("hellopreloader_preload");
-function fadeOutnojquery(el){
+var hellopreloader = document.getElementsByClassName("loader-container")[0];
+function fadeOutnojquery(el) {
     el.style.opacity = 1;
     var interhellopreloader = setInterval(
-        function(){
-            el.style.opacity = el.style.opacity - 0.05;
-            if (el.style.opacity <=0.05)
-                { 
-                    clearInterval(interhellopreloader);
-                    hellopreloader.style.display = "none";
-                }},16);
+        function () {
+            el.style.opacity = el.style.opacity - 0.025;
+            if (el.style.opacity <= 0.025) {
+                clearInterval(interhellopreloader);
+                hellopreloader.style.display = "none";
             }
-            window.onload = function(){
-                setTimeout(function()
-                {fadeOutnojquery(hellopreloader);
-            },1000);
-        };
+        }, 16);
+}
+window.onload = function () {
+    setTimeout(function () {
+        fadeOutnojquery(hellopreloader);
+    }, 1000);
+
+    
+};
